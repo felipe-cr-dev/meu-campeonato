2e37024fbb64595a16826dd69c53e47557ea9b60
Felipe Carvalho <luizfelipecr401@gmail.com>
2023-10-17 01:23:08 -0300

    Projeto Inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simulação de Partida</title>
+</head>
+<body>
+    <h1>Meu Campeonato de Futibol</h1>
+    <p>Insira os Times</p>
+
+    <form action="jogar.php" method="post">
+        Time 1 <br>
+        <input type="text" name="time1"> <br><br>
+        Time 2 <br>
+        <input type="text" name="time2"> <br><br>
+        <button type="submit">Iniciar partida</button>
+</body>
+</html>
